5365fcc169945947da1051bc348b86317f6aabe4
Israel <meir950216@gmail.com>
2020-02-03 09:27:06 +0100

    programme hello world

---
+++ hello.java
@@ -0,0 +1,15 @@
+/**
+ * Ceci est une implémentation du message traditionnel "Hello world!"
+ *
+ * @author L'équipe Education d'OpenClassrooms
+ */
+public class hello {
+
+    /**
+     * Le programme commence ici
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
